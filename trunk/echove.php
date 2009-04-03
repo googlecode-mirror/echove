e838162013238afbb49f5a91815734852d65634a
mattcongrove <mattcongrove@e643c2f0-f930-11dd-a8e2-f7cb3c35fcff>
2009-04-03 17:22:15 +0000

    Fix to remove notices. Added embed method. Corrected video lengths.

--- trunk/echove.php
+++ trunk/echove.php
@@ -1,48 +1,49 @@
 <?php
 
-/*********************************************************************
-
-	Echove, Brightcove PHP SDK
-
-	Official Website - http://echove.net
-	Code Repository - http://code.google.com/p/echove
-
-	Authors:
-		Matthew Congrove, Services Engineer, Brightcove, Inc
-		Brian Franklin, Services Engineer, Brightcove, Inc
-		
-	Version: Echove 0.2.1
-
-	Change Log:
-		0.2.1 - Setting default values to remove notices. Added inline
-				code documentation. Added utilities to convert video
-				lengths from milliseconds to formatted time or seconds
-				for ease of use and to convert video names to a
-				search-engine friendly format.
-		0.2.0 - Updated to include API return properties.
-		0.1.0 - Initial release.
-
-	Copyright 2009 Matthew Congrove, Brian Franklin
-
-	Permission is hereby granted, free of charge, to any person
-	obtaining a copy of this software and associated documentation
-	files (the "Software"), to deal in the Software without
-	restriction, including without limitation the rights to use,
-	copy, modify, merge, publish, distribute, sublicense, and/or
-	sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following
-	conditions:
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-	OTHER DEALINGS IN THE SOFTWARE.
-
-*********************************************************************/
+/**
+ * ECHOVE
+ * A Brightcove PHP SDK
+ * 
+ * Official Website:
+ *     http://echove.net/
+ * Code Repository:
+ *     http://code.google.com/p/echove/
+ * Authors:
+ *     Matthew Congrove, Prof. Services Engineer, Brightcove, Inc.
+ *     Brian Franklin, Prof. Services Engineer, Brightcove, Inc.
+ * Copyright:
+ *     Copyright (c) 2009, Matthew Congrove, Brian Franklin
+ * Version:
+ *     Echove 0.2.2 (03 APR 2009)
+ * Change Log:
+ *     0.2.2 - Fix to remove notices. Added embed method. Corrected
+ *             video lengths.
+ *     0.2.1 - Setting default values to remove notices. Added inline
+ *             code documentation. Added utilities to convert video
+ *             lengths from milliseconds to formatted time or seconds
+ *             for ease of use and to convert video names to a
+ *             search-engine friendly format.
+ *     0.2.0 - Updated to include API return properties.
+ *     0.1.0 - Initial release.
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 class Echove
 {
@@ -151,8 +152,10 @@ class Echove
 		if(is_array($params))
 		{
 			$url = $this->appendParams($method, $params);
-		} else {
+		} else if($default) {
 			$url = $this->appendParams($method, $params, $default);
+		} else {
+			$url = $this->appendParams($method, $params);
 		}
 
 		return $this->getData($url);
@@ -198,13 +201,13 @@ class Echove
 	{
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		$response = curl_exec($ch);
 		curl_close($ch);
 		
 		if($response)
 		{
-			if($response != 'null')
+			if($response != 'NULL')
 			{
 				$json = json_decode($response);
 
@@ -239,7 +242,7 @@ class Echove
     */
 	public function time($ms, $seconds = FALSE)
 	{
-		$total_seconds = ($ms / 1000) % 60;
+		$total_seconds = ($ms / 1000);
 			
 		if($seconds)
 		{
@@ -252,14 +255,14 @@ class Echove
 				'seconds' => 0
 			);
 			
-			if($total_seconds >= 3600){
+			if($total_seconds >= 3600) {
 				$value['hours'] = floor($total_seconds/3600);
 				$total_seconds = ($total_seconds%3600);
 
 				$time .= $value['hours'] . ':';
 			}
 			
-			if($total_seconds >= 60){
+			if($total_seconds >= 60) {
 				$value['minutes'] = floor($total_seconds/60);
 				$total_seconds = ($total_seconds%60);
 				
@@ -293,6 +296,74 @@ class Echove
 		$name = preg_replace('/[^a-zA-Z0-9]+/', '-', $name);
 		
 		return $name;
+	}
+	
+   /**
+    * Returns the JavaScript version of the player embed code
+    * @access Public
+    * @since 0.2.2
+    * @param int [$playerId] The ID of the player to embed
+    * @param mixed [$videoIds] The ID of the default video, or an array of video IDs
+    * @param array [$params] A key-value array of embed parameters
+    * @param array [$additional] A key-value array of additional embed parameters
+    * @return string The embed code
+    */
+	public function embed($playerId, $videoIds = NULL, $params = NULL, $additional = NULL)
+	{
+		$values = array('id' => 'myExperience', 'bgcolor' => 'FFFFFF', 'width' => 486, 'height' => 412);
+
+		foreach($values as $key => $value)
+		{
+			if(isset($params[$key]))
+			{
+				$values[$key] = $params[$key];
+			}
+		}
+		
+		$additionalCode = '';
+		
+		if(is_array($additional))
+		{
+			foreach($additional as $key => $value)
+			{
+				$additionalCode .= '<param name="' . $key . '" value="' . $value . '" />';
+			}
+		}
+		
+		$videoCode = '';
+		
+		if($videoIds)
+		{
+			if(is_array($videoIds))
+			{
+				$videoCode = '<param name="@videoPlayer" value="';
+				
+				foreach($videoIds as $videoId)
+				{
+					$videoCode .= $videoId . ',';
+				}
+				
+				$videoCode = substr($videoCode, 0, -1);
+				$videoCode .= '" />';
+			} else {
+				$videoCode = '<param name="@videoPlayer" value="' . $videoIds . '" />';
+			}
+		}
+		
+		$code = '
+			<object id="' . $values['id'] . '" class="BrightcoveExperience">
+				<param name="bgcolor" value="#' . $values['bgcolor'] . '" />
+				<param name="width" value="' . $values['width'] . '" />
+				<param name="height" value="' . $values['height'] . '" />
+				<param name="playerID" value="' . $playerId . '" />'
+				. $videoCode .
+				. $additionalCode . '
+				<param name="isVid" value="true" />
+				<param name="isUI" value="true" />
+			</object>
+		';
+		
+		return $code;
 	}
 
 }
